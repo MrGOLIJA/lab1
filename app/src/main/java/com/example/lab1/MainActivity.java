434da985d858ad681f7d8ef2b7140351cbfb8427
MrGOLIJA <sarsembaevgoergij@gmail.com>
2025-02-04 12:08:17 +0600

    asd

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView Text;
+    private LinearLayout Layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Text = findViewById(R.id.textView);
+        Layout = findViewById(R.id.Layout);
+    }
+    public void changeText(View view){
+        Text.setText("Фон не изменился, но я поменялся");
+        Layout.setBackgroundColor(getResources().getColor(R.color.bg));
     }
 }
